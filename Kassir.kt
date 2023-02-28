1e24cb3e59e3e832d8d6329be377158e13fbeee0
AydinAmiov <123439960+AydinAmiov@users.noreply.github.com>
2023-02-28 09:50:57 +0500

    Add files via upload

---
+++ Kassir.kt
@@ -0,0 +1,7 @@
+package zd_kassa
+open class Kassir(var station:String,var time:String,var data:String,var cost:Int){
+    open fun IsOver(){
+        if(cost<10) println("счет не оплачен")
+        else println("счет оплачен")
+    }
+}
